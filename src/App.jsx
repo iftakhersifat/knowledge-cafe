a166f91326cbfd02e38ccf7a44746b86e375ae8b
iftakhersifat <hossensifat615@gmail.com>
2025-04-06 15:49:16 +0600

    daisyUI setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
       <h1>knowledge-cafe</h1>
+      <Navbar></Navbar>
       
     </>
   )
