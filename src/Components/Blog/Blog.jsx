5ef651903953bec574272fa68b3b64d638e93099
iftakhersifat <hossensifat615@gmail.com>
2025-04-06 16:51:35 +0600

    added title

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Blog = ({blogger}) => {
     console.log(blogger)
-    const {cover}=blogger;
+    const {cover, title}=blogger;
     return (
         <div>
             <div className="card bg-base-100 w-96 shadow-sm">
@@ -12,10 +12,10 @@ const Blog = ({blogger}) => {
       alt="" />
   </figure>
   <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
+    <h2 className="card-title">{title}</h2>
     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-primary">mark as read</button>
     </div>
   </div>
 </div>
